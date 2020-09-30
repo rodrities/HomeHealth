3ac1dcbe0fb0fa4b1c8e63bd67005f8b082b6733
rodrities <54970340+rodrities@users.noreply.github.com>
2020-09-30 17:47:46 -0500

    Add files via upload

---
+++ homehealthy/src/main/java/com/acme/homehealthy/HomehealthyApplication.java
@@ -0,0 +1,13 @@
+package com.acme.homehealthy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomehealthyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomehealthyApplication.class, args);
+    }
+
+}
