0b41f286a8223d421a93f2e4986c34c1f670da52
LucianoLopez <55890030+FelCreepsXT@users.noreply.github.com>
2020-10-22 17:29:31 -0500

    add

---
+++ homehealthy/src/main/java/com/acme/homehealthy/Initialization/controller/CustomerController.java
@@ -0,0 +1,65 @@
+package com.acme.blogging.Initialization.controller;
+
+import com.acme.blogging.Initialization.domain.model.Collaborator;
+import com.acme.blogging.Initialization.domain.model.Customer;
+import com.acme.blogging.Initialization.domain.service.CustomerService;
+import com.acme.blogging.Initialization.resource.CollaboratorResource;
+import com.acme.blogging.Initialization.resource.CustomerResource;
+import com.acme.blogging.Initialization.resource.SaveCollaboratorResource;
+import com.acme.blogging.Initialization.resource.SaveCustomerResource;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Tag(name = "Customers", description = "Initialization API")
+@RestController
+@RequestMapping("api/")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @GetMapping("/customer/{customerId}")
+    public CustomerResource getCustomerByUsername(@Valid @PathVariable (value = "customerId") Long customerId){
+        return convertToResource(customerService.getCustomerById(customerId));
+    }
+
+    @GetMapping("/customers/{username}/{password}")
+    public CustomerResource getCustomerByUsernameAndPassword(   @Valid @PathVariable (value = "username") String username,
+                                                                @Valid @PathVariable (value = "password") String password){
+        return convertToResource(customerService.getCustomerByUsernameAndPassword(username,password));
+    }
+
+    @PostMapping("/customers")
+    public CustomerResource createCustomer(@Valid @RequestBody SaveCustomerResource resource){
+        Customer customer = convertToEntity(resource);
+        return convertToResource(customerService.createCustomer(customer));
+    }
+
+    @PutMapping("/customers/{customerId}")
+    public CustomerResource updateCustomer( @Valid @PathVariable (value = "customerId") Long customerId,
+                                            @Valid @RequestBody SaveCustomerResource resource){
+        Customer customer = convertToEntity(resource);
+        return convertToResource(customerService.updateCustomer(customerId,customer));
+    }
+
+    @DeleteMapping("/customers/{username}")
+    public ResponseEntity<?> deleteCustomer(@Valid @PathVariable (value = "username") String username){
+        return  customerService.deleteCustomer(username);
+    }
+
+    private Customer convertToEntity(SaveCustomerResource resource){
+        return mapper.map(resource, Customer.class);
+    }
+
+    private CustomerResource convertToResource(Customer customer){
+        return mapper.map(customer, CustomerResource.class);
+    }
+}
---
+++ homehealthy/src/main/java/com/acme/homehealthy/MemberShip/domain/model/OrderSubscription.java
@@ -0,0 +1,82 @@
+package com.acme.blogging.MemberShip.domain.model;
+
+import com.acme.blogging.Initialization.domain.model.Customer;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.sun.istack.NotNull;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "order_subscriptions")
+public class OrderSubscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "plan_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Plan plan;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Customer customer;
+
+    @NotNull
+    private Long amounthOfMonth;
+
+    @NotNull
+    private Long totalPrice;
+
+    public Long getId() {
+        return id;
+    }
+
+    public OrderSubscription setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public Plan getPlan() {
+        return plan;
+    }
+
+    public OrderSubscription setPlan(Plan plan) {
+        this.plan = plan;
+        return this;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public OrderSubscription setCustomer(Customer customer) {
+        this.customer = customer;
+        return this;
+    }
+
+    public Long getAmounthOfMonth() {
+        return amounthOfMonth;
+    }
+
+    public OrderSubscription setAmounthOfMonth(Long amounthOfMonth) {
+        this.amounthOfMonth = amounthOfMonth;
+        return this;
+    }
+
+    public Long getTotalPrice() {
+        return totalPrice;
+    }
+
+    public OrderSubscription setTotalPrice() {
+        this.totalPrice = plan.getPrice() * amounthOfMonth;
+        return this;
+    }
+
+}
