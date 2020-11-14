e3d52b988944b4a28bf3ea34bdeda1b94e010c16
PC <Lmaodiego@gmail.com>
2020-11-13 23:28:11 -0500

    ERRORES SOLUCIONADOS

--- homehealthy/src/main/java/com/acme/homehealthy/MemberShip/domain/repository/SubscriptionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.acme.homehealthy.MemberShip.domain.repository;
-
-import com.acme.homehealthy.MemberShip.domain.model.Subscription;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
-}
--- homehealthy/src/main/java/com/acme/homehealthy/Social/domain/repository/ComplaintRepository.java
+++
@@ -1,7 +0,0 @@
-package com.acme.homehealthy.Social.domain.repository;
-
-import com.acme.homehealthy.Social.domain.model.Complaint;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
-}
