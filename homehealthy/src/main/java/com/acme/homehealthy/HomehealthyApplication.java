b2fdea6b27bb6425a0b12a29dff3c4d105958e3b
rodrities <54970340+rodrities@users.noreply.github.com>
2020-11-19 03:00:00 -0500

    Add files via upload

--- homehealthy/src/main/java/com/acme/homehealthy/HomehealthyApplication.java
+++ homehealthy/src/main/java/com/acme/homehealthy/HomehealthyApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -17,5 +19,15 @@ public class HomehealthyApplication {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**");
+            }
+        };
     }
 }
